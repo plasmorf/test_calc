16e63d097311240e80c178497d59d7340e7f38c9
Администратор <plasmorf@mail.ru>
2015-09-19 16:35:13 +0600

    !

---
+++ Main/src/com/suhorukov/calculator/Main.java
@@ -0,0 +1,20 @@
+package com.suhorukov.calculator;
+
+import java.util.Scanner;
+
+/**
+ * Created by Администратор on 19.09.2015.
+ */
+public class Main {
+    public static void main(String[] args) { 
+
+        if (args[0])
+        {
+            Scanner scan = new Scanner();
+        }
+        else{
+
+        }
+        Calculator calc = new Calculator();
+    }
+}
