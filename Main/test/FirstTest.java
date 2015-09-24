9ad0d07f5c0f5d44d16cb5dd6916f6f60a6f9a83
Администратор <plasmorf@mail.ru>
2015-09-24 19:24:56 +0600

    2

---
+++ Main/test/FirstTest.java
@@ -0,0 +1,23 @@
+package com.suhorukov.calculator;
+import junit.framework.TestCase;
+/**
+ * Created by Администратор on 24.09.2015.
+ */
+public class FirstTest extends TestCase {
+    public void setUp() throws Exception{
+        super.setUp();
+    }
+    public void tearDown() throws Exception{
+        super.tearDown();
+    }
+
+    public void testMaxInt() throws  Exception{
+        int result = Main.maxInt(1,3);
+        assertFalse(1==result);
+    }
+
+    public void testAvgInt() throws Exception{
+        double result = Main.avg(2000,1000);
+        assertEquals(1500, result);
+    }
+}
